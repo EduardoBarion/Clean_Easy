1db06bb6896b6a9b3d2da46c040bba5cc8a4d1e0
Igor Fernandes <fernandesigor.bcn@icloud.com>
2024-08-27 17:41:36 -0300

    Workers controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :workers
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
