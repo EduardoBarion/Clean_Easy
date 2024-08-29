b7b4b5ffe04e2c964e92f7bca4d96cf9ea6b46ad
Igor Fernandes <fernandesigor.bcn@icloud.com>
2024-08-29 16:59:29 -0300

    pause on adding-bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :services do
     resources :bookings
   end
+
   resources :bookings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
